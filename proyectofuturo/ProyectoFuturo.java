51c64e0a1d918dcdda562e874aa6c8431e85f84d
Maite <mroccast@gmail.com>
2021-11-19 22:00:57 +0100

    Commit inicial

---
+++ proyectofuturo/ProyectoFuturo.java
@@ -0,0 +1,20 @@
+package proyectofuturo;
+
+public class ProyectoFuturo {
+    
+    public static String veredicto(String valor1, String valor2) {
+        if (valor1.equals(valor2)) {
+            return "Bien. Pero una hora de estudio para cada módulo puede ser insuficiente.";
+        }
+        if (Integer.valueOf(valor1) < Integer.valueOf(valor2)){
+            return "Poco tiempo de estudio. Debes dedicar más tiempo.";
+        }
+        return "Ideal. Trabajas los contenidos en casa.";
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("Estudiante " + args[0] + " : " + ProyectoFuturo.veredicto(args[1], args[2]));
+    
+    }
+    
+}
